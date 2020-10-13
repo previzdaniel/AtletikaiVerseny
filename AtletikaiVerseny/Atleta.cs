25ba82b2dcb4e99a55b14db7de8d21b8f8f96f8f
previzdaniel <dani.2000.2x@gmail.com>
2020-10-13 08:36:03 +0200

    2. feladat kész

---
+++ AtletikaiVerseny/Atleta.cs
@@ -0,0 +1,30 @@
+﻿namespace AtletikaiVerseny
+{
+    internal class Atleta
+    {
+        public string Rajtszam { get; private set; }
+        public string TeljesNev { get; private set; }
+        public string VezNev { get; private set; }
+        public string KerNev { get; private set; }
+        public string Egyesulet { get; private set; }
+        public int Ugras { get; private set; }
+
+        public void Nev(string nev)
+        {
+            string[] adat = nev.Split(' ');
+            VezNev = adat[0];
+            KerNev = adat[1];
+        }
+
+        public Atleta(string szoveg)
+        {
+            
+            string[] adat = szoveg.Split(';');
+            Rajtszam = adat[0];
+            TeljesNev = adat[1];
+            Egyesulet = adat[2];
+            Ugras = int.Parse(adat[3]);
+            Nev(TeljesNev);
+        }
+    }
+}
--- AtletikaiVerseny/Program.cs
+++ AtletikaiVerseny/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AtletikaiVerseny
 {
     class Program
     {
+        static List<Atleta> atletak = new List<Atleta>();
+
+        static void Beolvas()
+        {
+            StreamReader file = new StreamReader("tavol.csv");
+            while (!file.EndOfStream)
+            {
+                atletak.Add(new Atleta(file.ReadLine()));
+            }
+            file.Close();
+        }
+
+        static void feladat2()
+        {
+            Console.WriteLine("2. feladat: Nevek és ugrások\n");
+            foreach (var a in atletak)
+            {
+                Console.WriteLine($"{a.VezNev}\t{a.KerNev}\t{a.Ugras}");
+            }
+        }
+
+        static void feladat3()
+        {
+
+        }
+
         static void Main(string[] args)
         {
+            Beolvas();
+            feladat2();
+            feladat3();
+            //Atleta a = new Atleta("623;Ug Imre;Kalocsai Rozmarok;640");
+
+            Console.ReadKey();
         }
     }
 }
